50d2501c0202f68651feddfd316a500100096e4c
sun <sun@LAPTOP-MKCGS0B2>
2020-03-20 17:21:06 +0800

    第一次提交

---
+++ Iot1708A/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iot1708A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("sunyan");
+        }
+    }
+}
